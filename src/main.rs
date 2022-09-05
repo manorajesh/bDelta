2f8c8a50e60915a476c45d2069f242ed1df1665e
manorajesh <mano@raje.sh>
2022-09-05 09:18:53 -0700

    General formatting changes (no functional change)

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,23 @@
-use std::{fs::{remove_file, rename, OpenOptions, File}, io::{stdout, stdin, Read, Write}, path::Path, process::exit};
-use clap::{Parser, Subcommand};
-use zip::{ZipArchive, ZipWriter};
 use blake3::Hasher;
-/* 
- First, identify and record the differing bytes between the source and
- the new file
- Then, create a new temporary file and copy the source file with the 
- differing bytes at the correct locations as well.
- Finally, rename the temporary file to the new file.
- */
+use clap::{Parser, Subcommand};
+use std::{
+    fs::{remove_file, rename, File, OpenOptions},
+    io::{stdin, stdout, Read, Write},
+    path::Path,
+    process::exit,
+};
+use zip::{ZipArchive, ZipWriter};
+/*
+First, identify and record the differing bytes between the source and
+the new file
+Then, create a new temporary file and copy the source file with the
+differing bytes at the correct locations as well.
+Finally, rename the temporary file to the new file.
+*/
 
- // TODO
-    // 1. Add hashing to the diff to check if they are the same when applied DONE
-    // 2. Issue with context not being applied correctly
+// TODO
+// 1. Add hashing to the diff to check if they are the same when applied DONE
+// 2. Issue with context not being applied correctly
 
 #[derive(Parser)]
 #[clap(version = "0.1", author = "Mano Rajesh")]
@@ -34,7 +39,7 @@ enum SubCommands {
 
         #[clap(short, long)]
         request: bool,
-        
+
         #[clap(short, long = "dont-delete-diff", action, default_value = "false")]
         delete: bool,
     },
@@ -60,14 +65,23 @@ fn main() {
     let cli = Cli::parse();
 
     match cli.subcmd {
-        SubCommands::Apply { source, diff , request, mut delete} => {
-            let source = if Path::new(&source).exists() {&source[..]} else {
+        SubCommands::Apply {
+            source,
+            diff,
+            request,
+            mut delete,
+        } => {
+            let source = if Path::new(&source).exists() {
+                &source[..]
+            } else {
                 eprintln!("Source file does not exist");
                 exit(1);
             };
 
-            let diff_file = if Path::new(&diff).exists() {&diff[..]} else {
-                eprintln!("New file does not exist");
+            let diff_file = if Path::new(&diff).exists() {
+                &diff[..]
+            } else {
+                eprintln!("Diff file does not exist");
                 exit(1);
             };
 
@@ -77,19 +91,29 @@ fn main() {
                 delete = true;
             }
 
-            if !delete { // for readability - if 'dont-delete' is false
+            if !delete {
+                // for readability - if 'dont-delete' is false
                 remove_file(diff_file).expect("Failed to delete diff file");
                 println!("Deleted diff file");
             }
         }
-        SubCommands::Generate { source, new , output, print_stdout} => {
-            let source = if Path::new(&source).exists() {&source[..]} else {
+        SubCommands::Generate {
+            source,
+            new,
+            output,
+            print_stdout,
+        } => {
+            let source = if Path::new(&source).exists() {
+                &source[..]
+            } else {
                 eprintln!("Source file does not exist");
                 exit(1);
             };
 
-            let new = if Path::new(&new).exists() {&new[..]} else {
-                eprintln!("Diff file does not exist");
+            let new = if Path::new(&new).exists() {
+                &new[..]
+            } else {
+                eprintln!("New file does not exist");
                 exit(1);
             };
 
@@ -108,17 +132,25 @@ fn diff(file1: &str, file2: &str) -> Vec<(u64, u8, bool)> {
     let source_len = source.metadata().unwrap().len();
     let new_len = new.metadata().unwrap().len();
 
-    let max_character = if source_len < CHUNK_SIZE {source_len} else {CHUNK_SIZE};
+    let max_character = if source_len < CHUNK_SIZE {
+        source_len
+    } else {
+        CHUNK_SIZE
+    };
 
     let mut diff = Vec::new();
 
     println!("Finding diffs...");
-    
+
     let mut i: u64 = 0;
     let mut j: usize = 0;
     loop {
-        if new.read(&mut buffer2).expect("Unable to read file") == 0 {break} // break when EOF
-        if source.read(&mut buffer1).expect("Unable to read file") == 0 {break}
+        if new.read(&mut buffer2).expect("Unable to read file") == 0 {
+            break;
+        } // break when EOF
+        if source.read(&mut buffer1).expect("Unable to read file") == 0 {
+            break;
+        }
 
         if buffer1 != buffer2 {
             while i < max_character as u64 && j < max_character as usize {
@@ -135,7 +167,7 @@ fn diff(file1: &str, file2: &str) -> Vec<(u64, u8, bool)> {
             i += CHUNK_SIZE;
         }
     }
-    
+
     if new_len > source_len {
         let mut g = j;
         loop {
@@ -145,14 +177,17 @@ fn diff(file1: &str, file2: &str) -> Vec<(u64, u8, bool)> {
                 g += 1;
                 j += 1;
             }
-            if new.read(&mut buffer2).expect("Unable to read file") == 0 {break}
+            if new.read(&mut buffer2).expect("Unable to read file") == 0 {
+                break;
+            }
             g = 0;
         }
     } else if new_len < source_len {
         diff.push((new_len, 0, true));
     }
 
-    if diff != Vec::new() { // If there are no differences, return an empty vector
+    if diff != Vec::new() {
+        // If there are no differences, return an empty vector
         diff.insert(0, (new_len, 0, false)); // Add the length of the new file to the beginning of the vector
     }
     diff
@@ -169,41 +204,47 @@ fn apply(diff_bytes: Vec<(u64, u8, bool)>, target: &str, request: bool, hash: St
 
     // write to buffer file
     let mut bfile = OpenOptions::new()
-                            .read(true)
-                            .write(true)
-                            .create(true)
-                            .open(&buffer_file)
-                            .expect("Unable to open file");
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(&buffer_file)
+        .expect("Unable to open file");
 
     // target file to read from
     let mut tfile = OpenOptions::new()
-                            .read(true)
-                            .open(target)
-                            .expect("Unable to open file");
+        .read(true)
+        .open(target)
+        .expect("Unable to open file");
 
     let mut buffer = [0; CHUNK_SIZE as usize];
     let max_character = diff_bytes[0].0;
     diff_bytes.remove(0);
 
     println!("Applying patch...");
-    
+
     let mut i: u64 = 0;
-    
+
     diff_bytes.push((0, 0, true)); // Add a dummy value to the end of the vector to prevent out of bounds error
     'bufupdate: loop {
-        if tfile.read(&mut buffer).expect("Unable to read file") == 0 && i == max_character {break} // break when EOF
+        if tfile.read(&mut buffer).expect("Unable to read file") == 0 && i == max_character {
+            break;
+        } // break when EOF
 
         let mut g = 0;
         while i < max_character && g < CHUNK_SIZE {
             if diff_bytes[0].2 {
                 break 'bufupdate;
             } else if diff_bytes[0].0 == i {
-                bfile.write(&[diff_bytes[0].1]).expect("Unable to write to file");
+                bfile
+                    .write(&[diff_bytes[0].1])
+                    .expect("Unable to write to file");
                 diff_bytes.remove(0);
                 i += 1;
                 g += 1;
             } else {
-                bfile.write(&[buffer[i as usize]]).expect("Unable to write to file");
+                bfile
+                    .write(&[buffer[i as usize]])
+                    .expect("Unable to write to file");
                 i += 1;
                 g += 1;
             }
@@ -229,12 +270,12 @@ fn apply(diff_bytes: Vec<(u64, u8, bool)>, target: &str, request: bool, hash: St
             match usr_input.trim() {
                 "y" => {
                     break;
-                },
+                }
                 "n" => {
                     remove_file(&buffer_file).expect("Unable to remove file");
                     println!("{} file removed", buffer_file);
                     break;
-                },
+                }
                 _ => {
                     println!("Invalid input");
                     usr_input.clear();
@@ -273,12 +314,14 @@ fn compare_hash(hash1: &String, file: &str) -> bool {
     let mut buffer = [0; CHUNK_SIZE as usize];
 
     loop {
-        if file.read(&mut buffer).expect("Unable to read file") == 0 {break} // break when EOF
+        if file.read(&mut buffer).expect("Unable to read file") == 0 {
+            break;
+        } // break when EOF
         hasher.update(&buffer);
     }
 
     let hash2 = format!("{}", hasher.finalize().to_hex());
-    
+
     if *hash1 == hash2 {
         true
     } else {
@@ -301,30 +344,34 @@ fn serialize(diff: Vec<(u64, u8, bool)>, output_name: String, print_stdout: bool
     }
 
     let output = File::create(output_name).expect("Unable to create file");
-    
+
     let mut zip = ZipWriter::new(output);
 
     println!("\nZipping patch...");
 
-    zip.start_file("diff", Default::default()).expect("Unable to write to file");
+    zip.start_file("diff", Default::default())
+        .expect("Unable to write to file");
     for (i, byte, flag) in diff {
         write!(zip, "{:x},{:x},{}\n", i, byte, flag as u8).expect("Unable to write to file");
     }
 
     println!("\nGenerating hash...");
-    
-    // Genreate blake3 hash for new file and write to zip
+
+    // Generate blake3 hash for new file and write to zip
     let mut hasher = Hasher::new();
     let mut new_file = File::open(new_file).expect("Unable to open file");
     let mut buffer = [0; CHUNK_SIZE as usize];
     loop {
-        if new_file.read(&mut buffer).expect("Unable to read file") == 0 {break} // break when EOF
+        if new_file.read(&mut buffer).expect("Unable to read file") == 0 {
+            break;
+        } // break when EOF
         hasher.update(&buffer);
     }
 
     let hash = hasher.finalize();
 
-    zip.start_file("hash", Default::default()).expect("Unable to write to file");
+    zip.start_file("hash", Default::default())
+        .expect("Unable to write to file");
 
     println!("Hash: {}", hash.to_hex());
     write!(zip, "{}", hash.to_hex()).expect("Unable to write to file");
@@ -341,14 +388,17 @@ fn deserialize(zipped: &str) -> (Vec<(u64, u8, bool)>, String) {
 
             // Read the file
             let mut contents = String::new();
-            output.read_to_string(&mut contents).expect("Unable to read zip file");
+            output
+                .read_to_string(&mut contents)
+                .expect("Unable to read zip file");
             contents
-        },
+        }
         Err(_) => {
             // If the file is not a zip file, read it as a normal file
             let mut contents = String::new();
             let mut text = File::open(zipped).expect("Unable to open file");
-            text.read_to_string(&mut contents).expect("Unable to read file");
+            text.read_to_string(&mut contents)
+                .expect("Unable to read file");
             contents
         }
     };
@@ -359,12 +409,12 @@ fn deserialize(zipped: &str) -> (Vec<(u64, u8, bool)>, String) {
 
             // Read the file
             let mut contents = String::new();
-            output.read_to_string(&mut contents).expect("Unable to read zip file");
+            output
+                .read_to_string(&mut contents)
+                .expect("Unable to read zip file");
             contents
-        },
-        Err(_) => {
-            String::new()
         }
+        Err(_) => String::new(),
     };
 
     if hash == String::new() {
@@ -380,18 +430,17 @@ fn deserialize(zipped: &str) -> (Vec<(u64, u8, bool)>, String) {
             match usr_input.trim() {
                 "y" => {
                     break;
-                },
+                }
                 "n" => {
                     println!("Exiting...");
                     exit(0);
-                },
+                }
                 _ => {
                     println!("Invalid input");
                 }
             }
         }
     }
-
 
     let mut diff = Vec::new();
 
